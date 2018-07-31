459dd6a4d5dc4a74ae14d4ffc24f441dd47d74dd
unknown <someonefromcanada38@gmail.com>
2018-07-31 15:19:46 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TransferHttpCacheModule } from '@hapiness/ng-universal-transfer-http';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    // Add .withServerTransition() to support Universal rendering.
+    // The application ID can be any identifier which is unique on
+    // the page.
+    BrowserModule.withServerTransition({ appId: 'ng-universal-example' }),
+    // Add TransferHttpCacheModule to install a Http interceptor
+    TransferHttpCacheModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
