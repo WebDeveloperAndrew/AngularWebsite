644e596319de13ee6bc0050aabcefda7ff957c62
unknown <someonefromcanada38@gmail.com>
2018-08-02 13:08:38 -0400

    bootstrap 4 functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TransferHttpCacheModule } from '@hapiness/ng-universal-transfer-http';
-
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    TooltipModule.forRoot(),
     // Add .withServerTransition() to support Universal rendering.
     // The application ID can be any identifier which is unique on
     // the page.
-    BrowserModule.withServerTransition({ appId: 'ng-universal-example' }),
+    BrowserModule.withServerTransition({ appId: 'birdscreekfarmsupply' }),
     // Add TransferHttpCacheModule to install a Http interceptor
-    TransferHttpCacheModule
+    TransferHttpCacheModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
